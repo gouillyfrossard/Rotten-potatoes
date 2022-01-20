14fd8ed795e8f2d721ea18de447e8fe80847ed84
Guillaume Gouilly-Frossard <gouillyfrossard@gmail.com>
2022-01-20 16:42:55 +0100

    Add files via upload
    rotten-potatoes vue3

---
+++ nuxt.config.js
@@ -0,0 +1,81 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'rotten-potatoes',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: ['@/assets/defaults.scss'],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/bootstrap
+    'bootstrap-vue/nuxt',
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/auth',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+
+  // This tells NuxtJS that we have our API server running from /api directory using “index.js” file.
+  serverMiddleware: ['~/api/index.js'],
+
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          // these are the API endpoints we created in Express
+          login: {
+            url: '/api/users/login',
+            method: 'post',
+            propertyName: 'token',
+          },
+          logout: true,
+          user: {
+            url: '/api/users/user',
+            method: 'get',
+            propertyName: 'user',
+          },
+        },
+        tokenRequired: true,
+        tokenType: 'Bearer',
+      },
+    },
+    redirect: {
+      login: '/user/login', // User will be redirected to this path if login is required
+      logout: '/', // User will be redirected to this path if after logout, current route is protected
+      home: '/', // User will be redirect to this path after login if accessed login page directly
+    },
+    rewriteRedirects: true,
+  },
+}
